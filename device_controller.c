e1c1e1d39a178c0a2c958adc2655b4119f01491f
Hanon088 <62070088@kmitl.ac.th>
2022-09-16 21:08:14 +0700

    modified device_controller

--- device_controller.c
+++ device_controller.c
@@ -7,20 +7,29 @@
 
 unsigned int check_rules_in_device()
 {
+    // create kernel here? Or can we just queue it
     return NF_ACCEPT
 }
 
 int load_device_code()
 {
+    // load the device code into global var
     return 0
 }
 
 int load_rule_file()
 {
+    // load the rules from a file into global var
     return 0
 }
 
 int init_device()
 {
+    int err;
+    err = load_rule_file();
+    // then alloc rule into device
+
+    err = load_device_code();
+    // create program here
     return 0
 }
