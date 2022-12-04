4c0cf68d034130bb209aad7429499571c55fb307
Hanon088 <62070088@kmitl.ac.th>
2022-12-04 15:08:55 +0700

    Added working prototype

---
+++ opencl_firewall.c
@@ -0,0 +1,538 @@
+// c, cl headers
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <CL/cl.h>
+#include <stdbool.h>
+#include "src/src.h"
+#include <pthread.h>
+#include <linux/types.h>
+
+// nfq headers
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <linux/netfilter.h>
+#include <libnetfilter_queue/libnetfilter_queue.h>
+#include <libnetfilter_queue/pktbuff.h>
+#include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
+#include <libnetfilter_queue/libnetfilter_queue_tcp.h>
+
+#define ip_array_size 5
+#define rule_array_size 4
+
+/*struct nfq_data{
+    struct nfattr **data;
+};*/
+
+long int packet_count = 0;
+long int batch_num = 0;
+int netf_fd;
+char buf[0xffff] __attribute__((aligned));
+struct nfq_handle *handler;
+
+/*int ip_array_size = 20;
+int rule_array_size = 4;*/
+
+unsigned char string_ip[4];
+// uint32_t array_ip_input[ip_array_size];       // input ip array (uint32)
+uint32_t rule_ip[rule_array_size]; // input rule_ip (ip uint32)
+uint32_t mask[rule_array_size];    // input mask (mask uint32)
+int rule_verdict[rule_array_size];
+// bool result[ip_array_size * rule_array_size]; // output array order
+int result[ip_array_size]; // output array order
+
+// what if we can use pkt_buff instead
+struct callbackStruct
+{
+    struct nfq_q_handle *queue;
+    // struct nfgenmsg *nfmsg;
+    struct nfq_data *nfad;
+    // void *data;
+    struct callbackStruct *next;
+    uint32_t source_ip;
+    uint32_t dest_ip;
+    uint32_t packet_id;
+};
+
+struct callbackStruct *callbackStructArray[ip_array_size];
+struct callbackStruct *tailArray[ip_array_size];
+static pthread_mutex_t mtx[ip_array_size];
+static int packetNumInQ[ip_array_size];
+
+const char *source = "/home/tanate/github/opencl_firewalll/compare.cl";
+const char *func_compare = "compare";
+const char *func_sync = "sync_rule_and_verdict";
+
+// example value
+#define printable_ip(addr)           \
+    ((unsigned char *)&addr)[3],     \
+        ((unsigned char *)&addr)[2], \
+        ((unsigned char *)&addr)[1], \
+        ((unsigned char *)&addr)[0]
+
+static int netfilterCallback(struct nfq_q_handle *queue, struct nfgenmsg *nfmsg, struct nfq_data *nfad, void *data)
+{
+    int queueNum, rcv_len, err;
+    struct callbackStruct *localBuff, *lastBuff;
+    unsigned char *rawData;
+    struct pkt_buff *pkBuff;
+    struct iphdr *ip;
+    struct nfqnl_msg_packet_hdr *ph;
+    uint32_t source_ip, dest_ip;
+
+    localBuff = malloc(sizeof(struct callbackStruct));
+    lastBuff = NULL;
+
+    localBuff->nfad = malloc(sizeof(nfad));
+
+    localBuff->queue = queue;
+
+    memcpy(localBuff->nfad, nfad, sizeof(nfad));
+    localBuff->next = NULL;
+
+    memcpy(&queueNum, (int *)data, sizeof(int));
+    // printf("QUEUE NUM %d PACKET NUM %d\n", queueNum, packetNumInQ[queueNum] + 1);
+
+    ph = nfq_get_msg_packet_hdr(nfad);
+    if (!ph)
+    {
+        return 0;
+    }
+
+    rawData = NULL;
+    // get packet data from nfad
+    rcv_len = nfq_get_payload(nfad, &rawData);
+    if (rcv_len < 0)
+    {
+        nfq_set_verdict(queue, ntohl(ph->packet_id), NF_ACCEPT, 0, NULL);
+        return 0;
+    }
+
+    // allocate user space buffer???
+    pkBuff = pktb_alloc(AF_INET, rawData, rcv_len, 0x1000);
+    if (!pkBuff)
+    {
+        nfq_set_verdict(queue, ntohl(ph->packet_id), NF_ACCEPT, 0, NULL);
+        return 0;
+    }
+
+    ip = nfq_ip_get_hdr(pkBuff);
+    if (!ip)
+    {
+        nfq_set_verdict(queue, ntohl(ph->packet_id), NF_ACCEPT, 0, NULL);
+        return 0;
+    }
+
+    /*source_ip = ntohl(ip->saddr);
+    dest_ip = ntohl(ip->daddr);*/
+
+    localBuff->source_ip = ntohl(ip->saddr);
+    localBuff->dest_ip = ntohl(ip->daddr);
+    localBuff->packet_id = ntohl(ph->packet_id);
+
+    pktb_free(pkBuff);
+
+    if (!callbackStructArray[queueNum])
+    {
+        err = pthread_mutex_lock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_lock fails\n");
+            exit(1);
+        }
+        callbackStructArray[queueNum] = localBuff;
+        tailArray[queueNum] = localBuff;
+        packetNumInQ[queueNum]++;
+        err = pthread_mutex_unlock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_unlock fails\n");
+            exit(1);
+        }
+    }
+    else if (!tailArray[queueNum]->next)
+    {
+        err = pthread_mutex_lock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_lock fails\n");
+            exit(1);
+        }
+        tailArray[queueNum]->next = localBuff;
+        tailArray[queueNum] = tailArray[queueNum]->next;
+        packetNumInQ[queueNum]++;
+        err = pthread_mutex_unlock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_unlock fails\n");
+            exit(1);
+        }
+    }
+    else
+    {
+        // could this be causing trouble?
+        err = pthread_mutex_lock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_lock fails\n");
+            exit(1);
+        }
+        lastBuff = callbackStructArray[queueNum];
+
+        // what if lastBuff is freed by verdictThread before finding next?
+        while (lastBuff->next != NULL)
+        {
+            lastBuff = lastBuff->next;
+        }
+        lastBuff->next = localBuff;
+        tailArray[queueNum] = localBuff;
+        packetNumInQ[queueNum]++;
+        err = pthread_mutex_unlock(&mtx[queueNum]);
+        if (err != 0)
+        {
+            fprintf(stderr, "pthread_mutex_unlock fails\n");
+            exit(1);
+        }
+    }
+
+    return 0;
+}
+
+int compare_with_mask(uint32_t array_ip_input[], uint32_t rule_ip[], uint32_t mask[], int verdict[], int result[], int ip_arr_size, int rule_arr_size)
+{
+    // opencl structures
+    cl_device_id deviceId;
+    cl_context context;
+    cl_program program;
+    cl_kernel kernel_compare, kernel_sync;
+    cl_command_queue queue;
+    cl_int err;
+    bool first_result[ip_arr_size * rule_arr_size];
+    int rule_size[] = {rule_arr_size};
+    // Data and Buffer
+    cl_mem packet_buffer, rule_buffer, mask_buffer, verdict_buffer, output_buffer, result_buffer, rule_size_buffer;
+
+    // create cl_device
+    deviceId = create_device_cl();
+
+    long mem_size;
+    err = clGetDeviceInfo(deviceId, CL_DEVICE_GLOBAL_MEM_SIZE, sizeof(long), &mem_size, NULL);
+    printf("GLOBAL_MEM_SIZE : %ld MB \n", mem_size / 1000000);
+
+    // crate context
+    context = clCreateContext(NULL, 1, &deviceId, NULL, NULL, &err);
+    print_err(err);
+
+    // build program;
+    program = create_program_cl(context, deviceId, source);
+
+    // define 10 workgroup 1 local workgroup
+    size_t global_size[] = {rule_arr_size, ip_arr_size};
+    size_t local_size[] = {1, 1};
+    size_t global_offset[] = {0, 0};
+
+    // create data_buffer for read and write
+    packet_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, ip_arr_size * sizeof(uint32_t), array_ip_input, &err);
+    print_err(err);
+
+    rule_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, rule_arr_size * sizeof(uint32_t), rule_ip, &err);
+    print_err(err);
+
+    mask_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, rule_arr_size * sizeof(uint32_t), mask, &err);
+    print_err(err);
+
+    verdict_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, rule_arr_size * sizeof(int), verdict, &err);
+    print_err(err);
+
+    rule_size_buffer = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(int), rule_size, &err);
+    print_err(err);
+
+    output_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, rule_arr_size * ip_arr_size * sizeof(bool), first_result, &err);
+    print_err(err);
+
+    result_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR, ip_arr_size * sizeof(int), result, &err);
+    print_err(err);
+
+    queue = clCreateCommandQueueWithProperties(context, deviceId, 0, &err);
+    print_err(err);
+
+    // crate kernel_compare from source and func_compare
+    kernel_compare = clCreateKernel(program, func_compare, &err);
+    print_err(err);
+
+    // set kernel_compare function arguments
+    err = clSetKernelArg(kernel_compare, 0, sizeof(cl_mem), &packet_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_compare, 1, sizeof(cl_mem), &rule_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_compare, 2, sizeof(cl_mem), &mask_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_compare, 3, sizeof(cl_mem), &output_buffer);
+    print_err(err);
+
+    // Enqueue kernel_compare to device
+    err = clEnqueueNDRangeKernel(queue, kernel_compare, 2, global_offset, global_size, local_size, 0, NULL, NULL);
+    print_err(err);
+
+    // read result buffer in kernel_compare
+    err = clEnqueueReadBuffer(queue, output_buffer, CL_TRUE, 0, ip_arr_size * rule_arr_size * sizeof(bool), first_result, 0, NULL, NULL);
+    print_err(err);
+
+    kernel_sync = clCreateKernel(program, func_sync, &err);
+    print_err(err);
+
+    err = clSetKernelArg(kernel_sync, 0, sizeof(cl_mem), &output_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_sync, 1, sizeof(cl_mem), &verdict_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_sync, 2, sizeof(cl_mem), &result_buffer);
+    print_err(err);
+    err |= clSetKernelArg(kernel_sync, 3, sizeof(cl_mem), &rule_size_buffer);
+    print_err(err);
+
+    size_t sync_work_group[] = {ip_arr_size};
+    size_t sync_local[] = {1};
+
+    err = clEnqueueNDRangeKernel(queue, kernel_sync, 1, 0, sync_work_group, sync_local, 0, NULL, NULL);
+    print_err(err);
+
+    err = clEnqueueReadBuffer(queue, result_buffer, CL_TRUE, 0, ip_arr_size * sizeof(int), result, 0, NULL, NULL);
+    print_err(err);
+
+    // release resources
+    clReleaseKernel(kernel_compare);
+    clReleaseKernel(kernel_sync);
+    clReleaseMemObject(packet_buffer);
+    clReleaseMemObject(rule_buffer);
+    clReleaseMemObject(verdict_buffer);
+    clReleaseMemObject(output_buffer);
+    clReleaseMemObject(result_buffer);
+    clReleaseCommandQueue(queue);
+    clReleaseProgram(program);
+    clReleaseContext(context);
+    return 0;
+}
+
+void *verdictThread()
+{
+    int err;
+    uint32_t source_ip, dest_ip;
+    struct callbackStruct *tempNode;
+    uint32_t array_ip_input[ip_array_size];
+
+    while (1)
+    {
+        for (int i = 0; i < ip_array_size; i++)
+        {
+            if (!(callbackStructArray[i]))
+            {
+                goto cnt;
+            }
+        }
+
+        break;
+
+    cnt:;
+        continue;
+    }
+
+    while (1)
+    {
+        for (int i = 0; i < ip_array_size; i++)
+        {
+            if (!(callbackStructArray[i]))
+            {
+                goto cnt;
+            }
+
+            if (!(callbackStructArray[i]->next))
+            {
+                goto cnt;
+            }
+        }
+
+        printf("\n\n\nSTARTING OCL PREP %ld\n\n\n", ++batch_num);
+
+        for (int i = 0; i < ip_array_size; i++)
+        {
+            source_ip = callbackStructArray[i]->source_ip;
+            dest_ip = callbackStructArray[i]->dest_ip;
+            // printf("Q: %p NFAD %p\n", callbackStructArray[i]->queue, callbackStructArray[i]->nfad);
+            printf("QUEUE %d PACKET ID: %u\n", i, callbackStructArray[i]->packet_id);
+            printf("s %u.%u.%u.%u d %u.%u.%u.%u\n", printable_ip(source_ip), printable_ip(dest_ip));
+
+            array_ip_input[i] = source_ip;
+        }
+
+        // check rule_ip ip on cpu
+        bool test;
+        for (int i = 0; i < rule_array_size; i++)
+        {
+            printf("%s %d: %u.%u.%u.%u mask : %u.%u.%u.%u : verdict : %d\n", "rule_ip", i, printable_ip(rule_ip[i]), printable_ip(mask[i]), rule_verdict[i]);
+        }
+        for (int i = 0; i < ip_array_size * rule_array_size; i++)
+        {
+            test = rule_ip[i % rule_array_size] == (array_ip_input[i / rule_array_size] & mask[i % rule_array_size]);
+            printf("%d", test);
+            //        printf(" | %u.%u.%u.%u ", printable_ip(array_ip_input[i/rule_array_size]));
+            if (i % rule_array_size == rule_array_size - 1)
+            {
+                printf("\n");
+            }
+        }
+
+        // compare_with_mask(array_ip_input, rule_ip, mask, result, ip_array_size, rule_array_size);
+        compare_with_mask(array_ip_input, rule_ip, mask, rule_verdict, result, ip_array_size, rule_array_size);
+        /*for (int i = 0; i < sizeof(result) / sizeof(bool); i++)
+        {
+            printf("%d", result[i]);
+            if (i % rule_array_size == rule_array_size - 1)
+            {
+                printf("\n");
+            }
+        }*/
+        for (int i = 0; i < sizeof(result) / sizeof(int); i++)
+        {
+            printf("%d", result[i]);
+            nfq_set_verdict(callbackStructArray[i]->queue, callbackStructArray[i]->packet_id, result[i], 0, NULL);
+            err = pthread_mutex_lock(&mtx[i]);
+            if (err != 0)
+            {
+                fprintf(stderr, "pthread_mutex_lock fails\n");
+                exit(1);
+            }
+            if (callbackStructArray[i]->next)
+            {
+                tempNode = NULL;
+
+                tempNode = callbackStructArray[i];
+                callbackStructArray[i] = callbackStructArray[i]->next;
+                tempNode->queue = NULL;
+                free(tempNode->nfad);
+                free(tempNode);
+                packetNumInQ[i]--;
+            }
+            err = pthread_mutex_unlock(&mtx[i]);
+            if (err != 0)
+            {
+                fprintf(stderr, "pthread_mutex_unlock fails\n");
+                exit(1);
+            }
+        }
+    }
+}
+
+void *recvThread()
+{
+    int rcv_len;
+
+    while (1)
+    {
+        rcv_len = recv(netf_fd, buf, sizeof(buf), 0);
+        if (rcv_len < 0)
+            continue;
+        // printf("pkt received %ld\n", ++packet_count);
+        nfq_handle_packet(handler, buf, rcv_len);
+    }
+    return 0;
+}
+
+int main()
+{
+    struct nfq_q_handle *queue[ip_array_size];
+    pthread_t vt, rt;
+    int queueNum[ip_array_size];
+    struct callbackStruct *tempNode;
+
+    // initialize data copy ip and set rule_ip(uint32_t array)
+    for (int i = 0; i < rule_array_size; i++)
+    {
+        string_ip[3] = (unsigned int)192;
+        string_ip[2] = (unsigned int)168 + i;
+        string_ip[1] = (unsigned int)0;
+        string_ip[0] = (unsigned int)0;
+        memcpy(&rule_ip[i], string_ip, 4);
+        string_ip[3] = (unsigned int)255;
+        string_ip[2] = (unsigned int)255;
+        string_ip[1] = (unsigned int)0;
+        string_ip[0] = (unsigned int)0;
+        memcpy(&mask[i], string_ip, 4);
+        rule_verdict[i] = (i % 2 == 0);
+    }
+
+    for (int i = 0; i < ip_array_size; i++)
+    {
+        callbackStructArray[i] = NULL;
+        tailArray[i] = NULL;
+        mtx[i] = (pthread_mutex_t)PTHREAD_MUTEX_INITIALIZER;
+        packetNumInQ[i] = 0;
+    }
+
+    handler = nfq_open();
+
+    if (!handler)
+    {
+        fprintf(stderr, "error during nfq_open()\n");
+        exit(1);
+    }
+
+    // unbinding existing nf_queue handler for AF_INET (if any)
+    if (nfq_unbind_pf(handler, AF_INET) < 0)
+    {
+        fprintf(stderr, "error during nfq_unbind_pf()\n");
+        exit(1);
+    }
+
+    // binding nfnetlink_queue as nf_queue handler for AF_INET
+    if (nfq_bind_pf(handler, AF_INET) < 0)
+    {
+        fprintf(stderr, "error during nfq_bind_pf()\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < ip_array_size; i++)
+    {
+        queueNum[i] = i;
+        queue[i] = nfq_create_queue(handler, i, netfilterCallback, &queueNum[i]);
+        if (!queue[i])
+        {
+            fprintf(stderr, "error during nfq_create_queue()\n");
+            exit(1);
+        }
+
+        if (nfq_set_mode(queue[i], NFQNL_COPY_PACKET, 0xffff) < 0)
+        {
+            fprintf(stderr, "can't set packet_copy mode\n");
+            exit(1);
+        }
+    }
+
+    netf_fd = nfq_fd(handler);
+    pthread_create(&rt, NULL, recvThread, NULL);
+    pthread_create(&vt, NULL, verdictThread, NULL);
+
+    while (1)
+    {
+        continue;
+    }
+
+    for (int i = 0; i < ip_array_size; i++)
+    {
+        nfq_destroy_queue(queue[i]);
+        tempNode = callbackStructArray[i];
+        if (!tempNode)
+        {
+            continue;
+        }
+        while (tempNode->next != NULL)
+        {
+            tempNode = tempNode->next;
+            free(callbackStructArray[i]);
+            callbackStructArray[i] = tempNode;
+        }
+    }
+    nfq_close(handler);
+
+    return 0;
+}
