3bc1f4cb809bc098697a3cacaf72670be33f7174
Hanon088 <62070088@kmitl.ac.th>
2023-02-07 14:36:36 +0700

    Added required nfq udp library

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -16,6 +16,7 @@
 #include <libnetfilter_queue/pktbuff.h>
 #include <libnetfilter_queue/libnetfilter_queue_ipv4.h>
 #include <libnetfilter_queue/libnetfilter_queue_tcp.h>
+#include <libnetfilter_queue/libnetfilter_queue_udp.h>
 
 #include "variables.h"
 #include "compare.h"
