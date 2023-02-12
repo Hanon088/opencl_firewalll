6249dc03bf37f8fad95b147564ad3eae73a128b3
Hanon088 <62070088@kmitl.ac.th>
2023-02-12 15:40:55 +0700

    Added rule_loader module

---
+++ rule_loader.c
@@ -0,0 +1,158 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "compare.h"
+#include "variables.h"
+#include "rule_loader.h"
+
+// struct ipv4Rule *ruleList = NULL;
+//  char *ruleFileName = "C:\\Users\\Jack\\Documents\\Year 4 Project\\opencl_firewalll\\rules.txt";
+//   char *ruleFileName = "rules.txt";
+
+int parseIntoIPv4(char *ipStr, uint32_t *binIP)
+{
+    int bufferInt[4];
+    unsigned char bufferChr[4];
+    for (int i = 0; i < strlen(ipStr); i++)
+    {
+        if (ipStr[i] == '.')
+        {
+            ipStr[i] = ' ';
+        }
+    }
+    sscanf(ipStr, "%d %d %d %d", &bufferInt[3], &bufferInt[2], &bufferInt[1], &bufferInt[0]);
+    bufferChr[0] = (unsigned int)bufferInt[0];
+    bufferChr[1] = (unsigned int)bufferInt[1];
+    bufferChr[2] = (unsigned int)bufferInt[2];
+    bufferChr[3] = (unsigned int)bufferInt[3];
+    memcpy(binIP, bufferChr, 4);
+    return 0;
+}
+
+int parseRule(char *ruleString, struct ipv4Rule *ruleAddr)
+{
+    char sourceIP[16], sourceMask[16], destIP[16], destMask[16];
+    int sPort, dPort, protocol, verdict;
+    sscanf(ruleString, "%s %s %s %s %d %d %d %d", sourceIP, sourceMask, destIP, destMask, &sPort, &dPort, &protocol, &verdict);
+    parseIntoIPv4(sourceIP, &(ruleAddr->source_ip));
+    parseIntoIPv4(sourceMask, &(ruleAddr->source_ip_mask));
+    parseIntoIPv4(destIP, &(ruleAddr->dest_ip));
+    parseIntoIPv4(destMask, &(ruleAddr->dest_ip_mask));
+    ruleAddr->source_port = (unsigned int)sPort;
+    ruleAddr->dest_port = (unsigned int)dPort;
+    ruleAddr->ip_protocol = (unsigned int)protocol;
+    ruleAddr->verdict = verdict;
+    ruleAddr->next = NULL;
+
+    // assumes masks to be the data itself, for now
+    // masks are use to compare cases such as port=ALL
+    ruleAddr->source_port_mask = ruleAddr->source_port;
+    ruleAddr->dest_port_mask = ruleAddr->dest_port_mask;
+    ruleAddr->ip_protocol_mask = ruleAddr->ip_protocol;
+
+    return 0;
+}
+
+int load_rules(const char *filename, struct ipv4Rule *ruleList)
+{
+    FILE *ruleFile;
+    char *buffer;
+    size_t ruleSize;
+    char temp, rule[100];
+    int countBuff = 0, countRule = 0, ruleNum = 0;
+    ruleFile = fopen(filename, "r");
+    struct ipv4Rule *newRule, *tempRule = NULL;
+    int headLoaded = 0;
+    if (!ruleFile)
+    {
+        fprintf(stderr, "Rule File Not Found\n");
+        exit(1);
+    }
+    fseek(ruleFile, 0, SEEK_END);
+    ruleSize = ftell(ruleFile);
+    rewind(ruleFile);
+    buffer = (char *)malloc(ruleSize + 1);
+    buffer[ruleSize] = '\0';
+    fread(buffer, sizeof(char), ruleSize, ruleFile);
+    fclose(ruleFile);
+
+    while (countBuff < ruleSize)
+    {
+        temp = buffer[countBuff++];
+        if (temp == '\n' || temp == '\r')
+        {
+            continue;
+        }
+        if (temp == ';')
+        {
+            if (!headLoaded)
+            {
+                // ruleList = malloc(sizeof(struct ipv4Rule));
+                parseRule(rule, ruleList);
+                tempRule = ruleList;
+                headLoaded = 1;
+            }
+            else
+            {
+                newRule = malloc(sizeof(struct ipv4Rule));
+                parseRule(rule, newRule);
+                while (tempRule->next != NULL)
+                {
+                    tempRule = tempRule->next;
+                }
+
+                tempRule->next = newRule;
+            }
+            memset(buffer, 0, sizeof(buffer));
+            countRule = 0;
+            ruleNum++;
+            continue;
+        }
+        rule[countRule++] = temp;
+    }
+    free(buffer);
+    return ruleNum;
+}
+
+int freeRules(struct ipv4Rule *ruleList)
+{
+    struct ipv4Rule *temp = ruleList;
+
+    while (!temp)
+    {
+        ruleList = ruleList->next;
+        free(temp);
+        temp = ruleList;
+    }
+    return 0;
+}
+
+int ruleListToArr(struct ipv4Rule *ruleList, uint32_t *sAddr, uint32_t *sMask, uint32_t *dAddr, uint32_t *dMask, uint8_t *protoArr, uint16_t *sPortArr, uint16_t *dPortArr, int *verdictArr)
+{
+    struct ipv4Rule *temp = ruleList;
+    int count = 0;
+    while (1)
+    {
+
+        memcpy(&sAddr[count], &temp->source_ip, 4);
+        memcpy(&dAddr[count], &temp->dest_ip, 4);
+        memcpy(&sMask[count], &temp->source_ip_mask, 4);
+        memcpy(&dMask[count], &temp->dest_ip_mask, 4);
+
+        memcpy(&protoArr[count], &temp->ip_protocol, 1);
+        memcpy(&sPortArr[count], &temp->source_port, 2);
+        memcpy(&dPortArr[count], &temp->dest_port, 2);
+
+        memcpy(&verdictArr[count], &temp->verdict, sizeof(int));
+
+        if (!(temp->next))
+        {
+            break;
+        }
+        count++;
+        temp = temp->next;
+    }
+    return 0;
+}
---
+++ rule_loader.h
@@ -0,0 +1,33 @@
+#include <stdint.h>
+
+struct ipv4Rule
+{
+    /*
+    assumes transport layer uses tcp/udp for now
+    icmp types and others not taken into consideration
+    */
+
+    // source ip + dest ip = 64 bits
+    uint32_t source_ip;
+    uint32_t dest_ip;
+
+    // source port + dest port + protocol = 40 bits
+    uint16_t source_port;
+    uint16_t dest_port;
+    uint8_t ip_protocol;
+
+    // masks to convert the packets
+    uint32_t source_ip_mask;
+    uint32_t dest_ip_mask;
+
+    uint16_t source_port_mask;
+    uint16_t dest_port_mask;
+    uint8_t ip_protocol_mask;
+
+    int verdict;
+    struct ipv4Rule *next;
+};
+
+int load_rules(const char *filename, struct ipv4Rule *ruleList);
+int freeRules(struct ipv4Rule *ruleList);
+int ruleListToArr(struct ipv4Rule *ruleList, uint32_t *sAddr, uint32_t *sMask, uint32_t *dAddr, uint32_t *dMask, uint8_t *protoArr, uint16_t *sPortArr, uint16_t *dPortArr, int *verdictArr);
