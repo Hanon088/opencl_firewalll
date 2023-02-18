b6e177f9bda6c60fc5e20708c9c1b338f06e48e5
Hanon088 <62070088@kmitl.ac.th>
2023-02-18 14:14:16 +0700

    Modified pointers

--- compare.c
+++ compare.c
@@ -14,7 +14,7 @@ uint64_t mask[rule_array_size];
 int rule_verdict[rule_array_size];*/
 int result[ip_array_size];
 
-int compare_with_mask(uint64_t array_ip_input[], uint64_t rule_ip[], uint64_t mask[], int verdict[], int result[], int ip_arr_size, int rule_arr_size)
+int compare_with_mask(uint64_t array_ip_input[], uint64_t *rule_ip, uint64_t *mask, int *verdict, int result[], int ip_arr_size, int rule_arr_size)
 {
     // opencl structures
     cl_device_id deviceId;
