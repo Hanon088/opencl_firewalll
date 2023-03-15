09998b7dea2179557dc5683cd6c7795ceced3bc3
Hanon088 <62070088@kmitl.ac.th>
2023-03-15 14:51:26 +0700

    Changed opencl source file path

--- variables.c
+++ variables.c
@@ -1,7 +1,7 @@
 #include "variables.h"
 #include <stdint.h>
 
-const char *source = "/home/tanate/github/opencl_firewalll/compare.cl";
+const char *source = "/home/tanate/opencl_firewalll/compare.cl";
 const char *func_compare = "compare";
 const char *func_sync = "sync_rule_and_verdict";
 
