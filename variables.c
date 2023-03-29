d853bd0a6f7971c13f01cd99c5c4de1657540f6c
Hanon088 <62070088@kmitl.ac.th>
2023-03-29 12:32:40 +0700

    Forgot to save before commit

--- variables.c
+++ variables.c
@@ -1,8 +1,6 @@
 #include "variables.h"
 #include <stdint.h>
 
-static int queue_num = 5;
-static int queue_multipler = 2;
 const char *source = "/home/tanate/opencl_firewalll/compare.cl";
 const char *func_compare = "compare";
 const char *func_sync = "sync_rule_and_verdict";
