4ba4cef74df9240adf68a1ec1a1fa8e82bd93f70
Hanon088 <62070088@kmitl.ac.th>
2023-04-01 22:42:43 +0700

    Forgot to save file before add

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -379,11 +379,11 @@ void *verdictThread()
                 }
             }
         }
-        for (int i = 0; i < ip_array_size; i++)
+        /*for (int i = 0; i < ip_array_size; i++)
         {
             printf("IP %u.%u.%u.%u\n", printable_ip(array_ip_input[i]));
             printf("Proto %u sPort %u dPort %u\n", protocol_input[i], s_port_input[i], d_port_input[i]);
-        }
+        }*/
     }
 }
 
