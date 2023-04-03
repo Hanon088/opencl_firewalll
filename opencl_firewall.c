73264b4f3d20b89830fb48cc9b7dc0ecc8de1638
Hanon088 <62070088@kmitl.ac.th>
2023-04-03 14:08:09 +0700

    Confused

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -309,6 +309,12 @@ void *verdictThread()
             protocol_result, sport_result, dport_result;
         int verdict_buffer = 0;
 
+        for (int i = 0; i < ip_array_size; i++)
+        {
+            printf("IP %u.%u.%u.%u\n", printable_ip(array_ip_input[i]));
+            printf("Proto %u sPort %u dPort %u\n", protocol_input[i], s_port_input[i], d_port_input[i]);
+        }
+
         printf("MATCH ON CPU\n");
         for (int i = 0; i < ip_array_size * ruleNum; i++)
         {
