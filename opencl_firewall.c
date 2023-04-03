63de7cc2cff6762084035d31652d7ae38af78c3b
Hanon088 <62070088@kmitl.ac.th>
2023-04-03 14:22:21 +0700

    Printed out rules before matching

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -309,6 +309,13 @@ void *verdictThread()
             protocol_result, sport_result, dport_result;
         int verdict_buffer = 0;
 
+        print("\n");
+        for (int i = 0; i < ruleNum; i++)
+        {
+            printf("RULE s %d %u.%u.%u.%u d %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(rule_ip[i]), rule_protocol[i], rule_s_port[i], rule_d_port[i]);
+        }
+
+        print("\n");
         for (int i = 0; i < ip_array_size; i++)
         {
             printf("sIP %u.%u.%u.%u dIP %u.%u.%u.%u\n", printable_ip_joined(array_ip_input[i]));
