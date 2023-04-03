4559938d7762bb330453163cd6a687f47d2df509
Hanon088 <62070088@kmitl.ac.th>
2023-04-03 15:02:25 +0700

    If this works I'd need to relearn how the c compiler works

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -309,7 +309,7 @@ void *verdictThread()
             protocol_result, sport_result, dport_result;
         int verdict_buffer = 0;
 
-        /*printf("\n");
+        printf("\n");
         for (int i = 0; i < ruleNum; i++)
         {
             // printf("RULE s %d %u.%u.%u.%u d %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(rule_ip[i]), rule_protocol[i], rule_s_port[i], rule_d_port[i]);
@@ -321,7 +321,7 @@ void *verdictThread()
         {
             printf("sIP %u.%u.%u.%u dIP %u.%u.%u.%u\n", printable_ip_joined(array_ip_input[i]));
             printf("Proto %u sPort %u dPort %u\n", protocol_input[i], s_port_input[i], d_port_input[i]);
-        }*/
+        }
 
         printf("MATCH ON CPU\n");
         for (int i = 0; i < ip_array_size * ruleNum; i++)
@@ -578,11 +578,11 @@ int main()
     }
 
     // free rule arrays
-    free(rule_ip);
+    /*free(rule_ip);
     free(rule_mask);
     free(rule_verdict);
     free(rule_protocol);
     free(rule_s_port);
-    free(rule_d_port);
+    free(rule_d_port);*/
     return 0;
 }
