0d78d915998565689c42315eff1d66b03133811b
Hanon088 <62070088@kmitl.ac.th>
2023-04-04 09:48:00 +0700

    removed comment

--- opencl_firewall.c
+++ opencl_firewall.c
@@ -244,9 +244,6 @@ void *verdictThread()
     uint16_t *rule_d_port;
     int *rule_verdict;
 
-    // uint32_t *sAddr, *dAddr, *sMask, *dMask, mergeBuff[2] __attribute__((aligned));
-    //  uint16_t *sPort, *dPort;
-
     // packet data buffers
     int err;
     uint32_t ip_addr[2] __attribute__((aligned));
@@ -268,40 +265,14 @@ void *verdictThread()
     rule_d_port = malloc(ruleNum * 2);
     rule_verdict = malloc(ruleNum * sizeof(int));
 
-    // local buffers used to load rules
-    // sAddr = malloc(ruleNum * 4);
-    // dAddr = malloc(ruleNum * 4);
-    // sMask = malloc(ruleNum * 4);
-    // dMask = malloc(ruleNum * 4);
-    // sPort = malloc(ruleNum * 2);
-    // dPort = malloc(ruleNum * 2);
-
     printf("Number of rules %d\n", ruleNum);
     rule_list_to_arr_joined(ruleList, rule_ip, rule_mask, rule_protocol, rule_s_port, rule_d_port, rule_verdict);
     free_rule_list(ruleList);
 
-    /*loading procedure may be redundant but easier to modify if OpenCL arg size change, such as merging source and dest ip*/
-
     for (int i = 0; i < ruleNum; i++)
     {
         printf("RULE %d %u.%u.%u.%u d %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(rule_ip[i]), rule_protocol[i], rule_s_port[i], rule_d_port[i]);
-        /*mergeBuff[0] = sAddr[i];
-        mergeBuff[1] = dAddr[i];
-        memcpy(&rule_ip[i], mergeBuff, 8);
-        mergeBuff[0] = sMask[i];
-        mergeBuff[1] = dMask[i];
-        memcpy(&rule_mask[i], mergeBuff, 8);*/
     }
-    // memcpy(rule_s_port, sPort, ruleNum * 2);
-    // memcpy(rule_d_port, dPort, ruleNum * 2);
-
-    // free  local buffers
-    // free(sAddr);
-    // free(dAddr);
-    // free(sMask);
-    // free(dMask);
-    // free(sPort);
-    // free(dPort);
 
     // waits for packets to arrive in ALL queues
     while (1)
