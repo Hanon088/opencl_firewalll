d463a4c18fb59586199956a7e0cebed7849429be
Hanon088 <62070088@kmitl.ac.th>
2023-04-10 15:21:22 +0700

    Added rule gen for iptables test

---
+++ rule_gen_iptables.py
@@ -0,0 +1,24 @@
+"""
+Generate rule for iptables which will match all on last
+NOT TESTED YET
+"""
+
+import random
+
+s_ip = [f"127.0.{i}.{j}" for i in range(256) for j in range(256)]
+#s_mask = ["255.255.0.0" for _ in range(256*256)]
+d_ip = ["0.0.0.0" for _ in range(256*256)]
+#d_mask = ["0.0.0.0" for _ in range(256*256)]
+#proto = [["1", "6", "17"][i % 3] for i in range(256*256)]
+#s_port = ["0" for _ in range(256*256)]
+#d_port = list(random.sample(range(0, 2**16), 256*256))
+#verdict = [["0", "1"][i % 2] for i in range(256*256)]
+
+rules = [
+    f"iptables --insert FORWARD {i+1} --protocol all --src {s_ip[i]} --dst {d_ip[i]} --jump DROP\n" for i in range(256*256)]
+
+rules[-1] = "iptables --append FORWARD --protocol all --jump ACCEPT"
+rules = "".join(rules)
+
+with open('iptables_rules.txt', 'w') as f:
+    print(rules, file=f)
