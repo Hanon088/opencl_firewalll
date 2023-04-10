31393912071734d80b0b98a064c979e065e33384
Hanon088 <62070088@kmitl.ac.th>
2023-04-10 15:10:53 +0700

    Added new rule file to only match at the end, besides loopback 127.0.0.0/8

--- rule_gen.py
+++ rule_gen.py
@@ -1,6 +1,6 @@
 import random
 
-s_ip = [f"192.168.{i}.{j}" for i in range(256) for j in range(256)]
+s_ip = [f"127.0.{i}.{j}" for i in range(256) for j in range(256)]
 s_mask = ["255.255.0.0" for _ in range(256*256)]
 d_ip = ["0.0.0.0" for _ in range(256*256)]
 d_mask = ["0.0.0.0" for _ in range(256*256)]
@@ -12,7 +12,8 @@ verdict = [["0", "1"][i % 2] for i in range(256*256)]
 rules = [
     f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(256*256)]
 
+rules[-1] = "0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0 0 0 1 ;"
 rules = "".join(rules)
 
-with open('rules2.txt', 'w') as f:
+with open('rules3.txt', 'w') as f:
     print(rules, file=f)
