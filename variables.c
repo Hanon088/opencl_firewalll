fb433bd9c3690dbefbd8e8e942ddb666906de74d
Hanon088 <62070088@kmitl.ac.th>
2023-04-19 14:59:58 +0700

    Modified test vars

--- variables.c
+++ variables.c
@@ -2,11 +2,11 @@
 #include <stdint.h>
 #include <CL/cl.h>
 
-const char *source = "/home/tanate/opencl_firewalll/compare.cl";
+const char *source = "compare.cl";
 const char *func_compare = "compare";
 const char *func_sync = "sync_rule_and_verdict";
 
-const char *rule_file = "rules4.txt";
+const char *rule_file = "rules.txt";
 
 // opencl buffers
 cl_mem rule_ip_buffer,
--- variables.h
+++ variables.h
@@ -1,8 +1,8 @@
 #include <CL/cl.h>
 
-#define ip_array_size 90
+#define ip_array_size 12
 #define queue_num 6
-#define queue_multipler 15
+#define queue_multipler 2
 
 #define printable_ip(addr)           \
     ((unsigned char *)&addr)[3],     \
