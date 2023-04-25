2b0d335b69153af424ca0419e345d07900bbbab4
Hanon088 <62070088@kmitl.ac.th>
2023-04-25 09:05:54 +0700

    Added more rule files

--- rule_gen.py
+++ rule_gen.py
@@ -1,19 +1,19 @@
 import random
 
-s_ip = [f"127.0.{i}.{j}" for i in range(40) for j in range(256)]
-s_mask = ["255.255.0.0" for _ in range(40*256)]
-d_ip = ["0.0.0.0" for _ in range(40*256)]
-d_mask = ["0.0.0.0" for _ in range(40*256)]
-proto = [["1", "6", "17"][i % 3] for i in range(40*256)]
-s_port = ["0" for _ in range(40*256)]
-d_port = list(random.sample(range(0, 2**16), 40*256))
-verdict = [["0", "1"][i % 2] for i in range(40*256)]
+s_ip = [f"127.0.{i}.{j}" for i in range(240) for j in range(250)]
+s_mask = ["255.255.0.0" for _ in range(240*250)]
+d_ip = ["0.0.0.0" for _ in range(240*250)]
+d_mask = ["0.0.0.0" for _ in range(240*250)]
+proto = [["1", "6", "17"][i % 3] for i in range(240*250)]
+s_port = ["0" for _ in range(240*250)]
+d_port = list(random.sample(range(0, 2**16), 240*250))
+verdict = [["0", "1"][i % 2] for i in range(240*250)]
 
 rules = [
-    f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(40*256)]
+    f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(240*250)]
 
 rules[-1] = "0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0 0 0 1 ;"
 rules = "".join(rules)
 
-with open('rules4.txt', 'w') as f:
+with open('rules_60k.txt', 'w') as f:
     print(rules, file=f)
