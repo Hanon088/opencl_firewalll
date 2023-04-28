c9e28ecfb99f624f0b579c0b94bf6b9c1e3fdd49
Hanon088 <62070088@kmitl.ac.th>
2023-04-28 13:55:48 +0700

    Modified queue num

--- variables.h
+++ variables.h
@@ -1,8 +1,8 @@
 #include <CL/cl.h>
 
 #define ip_array_size 100
-#define queue_num 4
-#define queue_multipler 25
+#define queue_num 1
+#define queue_multipler 100
 
 #define printable_ip(addr)           \
     ((unsigned char *)&addr)[3],     \
