5416893dbd1d6c19e374d59d900d02344ea8a1f6
Hanon088 <62070088@kmitl.ac.th>
2023-04-28 14:52:37 +0700

    Modified test vars

--- variables.h
+++ variables.h
@@ -1,8 +1,8 @@
 #include <CL/cl.h>
 
-#define ip_array_size 100
+#define ip_array_size 10
 #define queue_num 1
-#define queue_multipler 100
+#define queue_multipler 10
 
 #define printable_ip(addr)           \
     ((unsigned char *)&addr)[3],     \
