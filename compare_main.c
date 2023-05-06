5cf65f1eb83a64c78db893d37d93000a1d8d53e5
Hanon088 <62070088@kmitl.ac.th>
2023-05-06 18:12:33 +0700

    Modified output

--- compare_main.c
+++ compare_main.c
@@ -64,16 +64,20 @@ int main()
     // wearied output on cpu but in opencl work fine
     int test,
         protocol_result, sport_result, dport_result;
-    printf("\tsIP\t\tdIP\t\tsMask\t\tdMask\t\tProto\tsPort\tdPort\tVerdict\n\n");
+    /*printf("\tsIP\t\tdIP\t\tsMask\t\tdMask\t\tProto\tsPort\tdPort\tVerdict\n\n");
     for (int i = 0; i < ruleNum; i++)
     {
         printf("RULE %d\t%u.%u.%u.%u\t%u.%u.%u.%u\t\t%u.%u.%u.%u\t\t%u.%u.%u.%u\t\t%d\t%u\t%u\t%d\n", i, printable_ip_joined(rule_ip[i]), printable_ip_joined(rule_mask[i]), rule_protocol[i], rule_s_port[i], rule_d_port[i], rule_verdict[i]);
+    }*/
+    for (int i = 0; i < ip_array_size; i++)
+    {
+        printf("Rule %d s %u.%u.%u.%u d %u.%u.%u.%u sm %u.%u.%u.%u dm %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(rule_ip[i]), printable_ip_joined(rule_mask[i]), rule_protocol[i], rule_s_port[i], rule_d_port[i], rule_verdict[i]);
     }
     printf("\n--------------------------------------\n\n");
     // printf("PACKETS CPU\n");
     for (int i = 0; i < ip_array_size; i++)
     {
-        printf("PACKET %d s %u.%u.%u.%u d %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(input_ip[i]), input_protocol[i], input_sport[i], input_dport[i]);
+        printf("Packet %d s %u.%u.%u.%u d %u.%u.%u.%u proto %d sp %u dp %u\n", i, printable_ip_joined(input_ip[i]), input_protocol[i], input_sport[i], input_dport[i]);
     }
     /*printf("\nPACKETS GPU\n");
     for (int i = 0; i < ip_array_size; i++)
