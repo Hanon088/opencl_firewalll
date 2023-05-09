b7fc60ab2044d5d0676067a77f1a2f581591fcac
Hanon088 <54878061+Hanon088@users.noreply.github.com>
2023-05-09 14:10:48 +0700

    Update rule_gen.py
    Modified vars

--- rule_gen.py
+++ rule_gen.py
@@ -1,16 +1,16 @@
 import random
 
-s_ip = [f"127.0.{i}.{j}" for i in range(36) for j in range(250)]
-s_mask = ["255.255.0.0" for _ in range(36*250)]
-d_ip = ["0.0.0.0" for _ in range(36*250)]
-d_mask = ["0.0.0.0" for _ in range(36*250)]
-proto = [["1", "6", "17"][i % 3] for i in range(36*250)]
-s_port = ["0" for _ in range(36*250)]
-d_port = list(random.sample(range(0, 2**16), 36*250))
-verdict = [["0", "1"][i % 2] for i in range(36*250)]
+s_ip = [f"127.0.{i}.{j}" for i in range(8) for j in range(256)]
+s_mask = ["255.255.0.0" for _ in range(8*256)]
+d_ip = ["0.0.0.0" for _ in range(8*256)]
+d_mask = ["0.0.0.0" for _ in range(8*256)]
+proto = [["1", "6", "17"][i % 3] for i in range(8*256)]
+s_port = ["0" for _ in range(8*256)]
+d_port = list(random.sample(range(0, 2**16), 8*256))
+verdict = [["0", "1"][i % 2] for i in range(8*256)]
 
 rules = [
-    f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(36*250)]
+    f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(8*256)]
 
 rules[-1] = "0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0 0 0 1 ;"
 rules = "".join(rules)
