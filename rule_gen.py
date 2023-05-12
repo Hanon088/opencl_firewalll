153b68a718c1f81b3bfc9bf935cbc6e99b7c84e1
Hanon088 <62070088@kmitl.ac.th>
2023-05-12 13:03:57 +0700

    Added more rules

---
+++ rule_gen.py
@@ -0,0 +1,19 @@
+import random
+
+s_ip = [f"127.0.{i}.{j}" for i in range(40) for j in range(250)]
+s_mask = ["255.255.0.0" for _ in range(40*250)]
+d_ip = ["0.0.0.0" for _ in range(40*250)]
+d_mask = ["0.0.0.0" for _ in range(40*250)]
+proto = [["1", "6", "17"][i % 3] for i in range(40*250)]
+s_port = ["0" for _ in range(40*250)]
+d_port = list(random.sample(range(0, 2**16), 40*250))
+verdict = [["0", "1"][i % 2] for i in range(40*250)]
+
+rules = [
+    f"{s_ip[i]} {s_mask[i]} {d_ip[i]} {d_mask[i]} {proto[i]} {s_port[i]} {d_port[i]} {verdict[i]} ;\n" for i in range(40*250)]
+
+rules[-1] = "0.0.0.0 0.0.0.0 0.0.0.0 0.0.0.0 0 0 0 1 ;"
+rules = "".join(rules)
+
+with open('rules\\rules_10k.txt', 'w') as f:
+    print(rules, file=f)
