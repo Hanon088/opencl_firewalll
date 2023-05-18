20622b3b5bbcae1622e8ed4dfcc4bc01e3d1c9a0
Hanon088 <62070088@kmitl.ac.th>
2023-05-18 11:18:58 +0700

    Added new rules

--- rule_gen_iptables.py
+++ rule_gen_iptables.py
@@ -7,7 +7,7 @@ import random
 
 s_ip = [f"10.168.{i}.{j}" for i in range(40) for j in range(250)]
 #s_mask = ["255.255.0.0" for _ in range(256*256)]
-d_ip = ["0.0.0.0" for _ in range(40*250)]
+d_ip = ["0.0.0.0/0" for _ in range(40*250)]
 #d_mask = ["0.0.0.0" for _ in range(256*256)]
 #proto = [["1", "6", "17"][i % 3] for i in range(256*256)]
 #s_port = ["0" for _ in range(256*256)]
@@ -20,5 +20,5 @@ rules = [
 rules[-1] = "iptables --append FORWARD --protocol all --jump ACCEPT"
 rules = "".join(rules)
 
-with open('iptables_rules_10k_new', 'w') as f:
+with open('iptables_rules_10k_new2', 'w') as f:
     print(rules, file=f)
