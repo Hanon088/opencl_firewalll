0c64f7d450a5da3314ca72e2c8b1e8d9f0ee8c73
Hanon088 <62070088@kmitl.ac.th>
2023-05-19 16:35:41 +0700

    Modifed vars

--- variables.h
+++ variables.h
@@ -1,8 +1,8 @@
 #include <CL/cl.h>
 
-#define ip_array_size 10
+#define ip_array_size 90
 #define queue_num 1
-#define queue_multipler 10
+#define queue_multipler 90
 
 #define printable_ip(addr)           \
     ((unsigned char *)&addr)[3],     \
